c1f4979ba0093d62e36f557ca1003d792107e4ae
whysoxmritx <143788953+whysoxmritx@users.noreply.github.com>
2024-08-24 15:29:24 +0530

    new

---
+++ Ques2.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int g1,g2,g3,g4,g5;
+    cin>>g1;
+    cin>>g2;
+    cin>>g3;
+    cin>>g4;
+    cin>>g5;
+    double avg=((g1+g2+g3+g4+g5)/5);
+    cout<<"The Average Grade is: "<<avg;
+}
