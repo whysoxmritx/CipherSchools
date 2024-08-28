7424c212d985909531bca16b560a8ed638a32e8d
whysoxmritx <143788953+whysoxmritx@users.noreply.github.com>
2024-08-28 18:49:49 +0530

    new

---
+++ Day2Q3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    switch(n)
+    {
+        case 1:cout<<"The Month is January";
+        break;
+        case 2:cout<<"The Month is Febuaray";
+        break;
+        case 3:cout<<"The Month is March";
+        break;
+        case 4:cout<<"The Month is April";
+        break;
+        case 5:cout<<"The Month is May";
+        break;
+        case 6:cout<<"The Month is June";
+        break;
+        case 7:cout<<"The Month is July";
+        break;
+        case 8:cout<<"The Month is August";
+        break;
+        case 9:cout<<"The Month is September";
+        break;
+        case 10:cout<<"The Month is October";
+        break;
+        case 11:cout<<"The Month is November";
+        break;
+        case 12:cout<<"The Month is December";
+        break;
+        default:cout<<"Invalid";
+    }
+}
